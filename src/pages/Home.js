429317c7fc52df195a247f9aae924c2bf55b10df
Grant Harris <87549967+GrantHarris1@users.noreply.github.com>
2022-01-30 17:36:15 -0500

    start3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {Canvas} from '@react-three/fiber';
+import Box from '../components/Box';
+import './Home.css';
+import { OrbitControls } from '@react-three/drei';
 
 export default function Home() {
-  return <div>
-      <h1>Grant Harris</h1>
-  </div>;
+  return <Canvas className='canvas'>
+    <OrbitControls  enableZoom={false}/>
+    <ambientLight intensity={0.5} />
+    <directionalLight position={[-2, 5, 2]} intensity={1} />
+    <Box />
+  </Canvas>
 }
+
+
